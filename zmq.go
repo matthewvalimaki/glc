25cea0fa3154d56a8f34e7f3f6d1492bae35762a
Matthew Valimaki <matthew.valimaki@gmail.com>
2015-09-14 10:25:43 -0700

    Fix how proxy is setup

--- zmq.go
+++ zmq.go
@@ -7,7 +7,7 @@ import (
 
 func BindProxy(frontend string, backend string) {
 	// create XSUB for publishers to connect to
-	xSub, _ := zmq4.NewSocket(zmq4.XSUB)
+	xSub, _ := zmq.NewSocket(zmq.XSUB)
 	defer xSub.Close()
 	err := xSub.Bind(frontend)
 	if err != nil {
@@ -15,19 +15,14 @@ func BindProxy(frontend string, backend string) {
     }
 	
 	// create XPUB for subscribers to connect to
-	xPub, _ := zmq4.NewSocket(zmq4.XPUB)
+	xPub, _ := zmq.NewSocket(zmq.XPUB)
 	defer xPub.Close()
 	err = xPub.Bind(backend)
 	if err != nil {
         log.Fatal(err)
     }
 
-	// create the proxy
-	go proxy(xSub, xPub)
-}
-
-func proxy(frontend *zmq.Socket, backend *zmq.Socket) {
-	err := zmq.Proxy(frontend, backend, nil)
+	err = zmq.Proxy(xSub, xPub, nil)
 	log.Fatalln("Proxy interrupted:", err)
 }
 
@@ -48,7 +43,7 @@ func BindPublisher(endpoint string) *zmq.Socket {
 	return socket
 }
 
-func BindSubscriber(endpoint string, filter string, callback func) *zmq.Socket {
+func BindSubscriber(endpoint string, filter string, callback func(message string)) *zmq.Socket {
 	socket, err := zmq.NewSocket(zmq.PUB)
 	defer socket.Close()
 	
